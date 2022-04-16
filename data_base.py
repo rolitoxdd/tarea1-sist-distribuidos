2b9fe10ed975c93f711a2fc5da9fce8b2a1f4bb5
xovalef <valeria.fuentes1@mail.udp.cl>
2022-04-15 23:11:12 -0400

    sql initial2

--- data_base.py
+++ data_base.py
@@ -1,32 +1,35 @@
 import psycopg2
 from environs import Env
 
+class Database():
+    
+    def connectionDB(self):
+        env = Env()
+        env.read_env()
+        try:
+            self.connection = psycopg2.connect(
+                dbname=env('POSTGRES_DATABASE'),
+                user=env('POSTGRES_USER'),
+                password=('POSTGRES_PASSWORD'),
+                host=env('POSTGRES_HOST')
+            )
+        except:
+            print ('ERROR AL CONECTAR')
 
+    def list_of_elements(self):
 
-def connectionDB():
-    env = Env()
-    env.read_env()
-    try:
-        return psycopg2.connect(
-            dbname=env('POSTGRES_DATABASE'),
-            user=env('POSTGRES_USER'),
-            password=('POSTGRES_PASSWORD'),
-            host=env('POSTGRES_HOST')
-        )
-    except:
-        print ('ERROR AL CONECTAR')
+        cursor= self.connection.cursor()
+        cursor.execute ("""SELECT * FROM products;""")
+        fet=cursor.fetchall()
+        return fet
 
-def list_of_elements():
+    def list_by_name(self,name_product):
 
-    connection=connectionDB
-    cursor= connection.cursor()
-    cursor.execute ("""SELECT * FROM products;""")
-    fet=cursor.fetchall()
-    return fet
-
-def list_by_name(name_product):
-    connection=connectionDB
-    cursor= connection.cursor()
-    cursor.execute (f"""SELECT * FROM products WHERE name = {name_product} ;""")
-    fet=cursor.fetchall()
-    return fet
+        cursor= self.connection.cursor()
+        cursor.execute (f"""SELECT * FROM products WHERE name = {name_product} ;""")
+        fet=cursor.fetchall()
+        return fet
+    
+db = Database()
+db.connectionDB()
+db.list_of_elements('hola')
